3ab9270c8ed94a27ae4c93f3b1cda393b93461a1
praiseopara <cpopara2010@yahoo.com>
2021-09-02 04:48:46 -0500

    created the comment model,routes and controllers,connected comments to pizzas,used getters and virtuals, and hooked up the frontend

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,6 +6,12 @@ const pizzaController = {
     //get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -16,18 +22,23 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => {
-                // if no pizza is found, send 404
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'no pizza found with this id!' });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbPizzaData => {
+            // if no pizza is found, send 404
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'no pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
 
     // create pizza
